cc674f84210856ec3276ec14f5fbf8fd4afa4d0d
omochi-star <omochim275@gmail.com>
2026-03-09 20:45:46 +0900

    レイアウト共通化

---
+++ src/main/java/com/example/task_manager/repository/task/TaskRepository.java
@@ -0,0 +1,4 @@
+package com.example.task_manager.repository.task;
+
+public class TaskRepository {
+}
---
+++ src/main/java/com/example/task_manager/service/task/TaskService.java
@@ -0,0 +1,9 @@
+package com.example.task_manager.service.task;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TaskService {
+    public static record TaskEntity() {
+    }
+}
